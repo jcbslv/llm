153338595fd72bdd4548ecf621cc6a65e8942a43
jcb <0x0jcb@gmail.com>
2024-12-21 17:10:50 -0600

    more models

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import matplotlib.pyplot as plt
 
-model = SentenceTransformer('sentence-transformers/average_word_embeddings_komninos')
+model = SentenceTransformer(
+    "sentence-transformers/gtr-t5-xl")
+    # trust_remote_code=True)
+    # device="cpu",
+    # config_kwargs={"use_memory_efficient_attention": False, "unpad_inputs": False})
 
 with open('Justifications1Col.txt', 'r') as file:
     documents = file.readlines()
@@ -35,7 +39,7 @@ for i in range(0, len(documents), 4):
     plt.yticks(ticks=np.arange(len(chunk)), labels=vector_labels)
 
     plt.tight_layout()
-    plt.savefig(f'komninos/plot{i}.png')  # Save the plot to a file
+    plt.savefig(f'gtr-t5-xl/plot{i}.png')  # Save the plot to a file
 
 
 
