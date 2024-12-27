9fce8684bd5b384d71697cdd6100036628ea0cf1
jcb <0x0jcb@gmail.com>
2024-12-27 01:21:28 -0600

    results

--- main.py
+++ main.py
@@ -2,42 +2,50 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import matplotlib.pyplot as plt
 
-model = SentenceTransformer(
+modelName = str(
     # 'thenlper/gte-base')
     # 'thenlper/gte-large')
     # 'Mihaiii/gte-micro')
     # "Mihaiii/Ivysaur")
+    # 'Snowflake/snowflake-arctic-embed-l-v2.0')
     # 'sentence-transformers/average_word_embeddings_glove.6B.300d')
-    'sentence-transformers/average_word_embeddings_komninos')
+    # 'sentence-transformers/average_word_embeddings_komninos')
     # 'sentence-transformers/all-mpnet-base-v2')
-    # 'sentence-transformers/all-MiniLM-L12-v2')
+    'sentence-transformers/all-MiniLM-L12-v2')
     # 'sentence-transformers/sentence-t5-xl')
     # 'sentence-transformers/gtr-t5-xl')
+    # "dunzhang/stella_en_1.5B_v5")
+    # "dunzhang/stella_en_400M_v5")
+   
+model = SentenceTransformer(modelName)
     # "dunzhang/stella_en_1.5B_v5", trust_remote_code=True)
     # "dunzhang/stella_en_400M_v5",
     # trust_remote_code=True,
     # device="cpu",
     # config_kwargs={"use_memory_efficient_attention": False, "unpad_inputs": False})
+    
 
 with open('Justifications1Col.txt', 'r') as file:
     documents = file.readlines()
 
 documents = [line.strip() for line in documents]
-# for line in documents:
-#     print(line)
 
 for i in range(0, len(documents), 4):
     chunk = documents[i:i+4]
     for doc in chunk:
         print(doc)
     embeddings = model.encode(chunk, normalize_embeddings=True)
-    print('\n',type(embeddings), embeddings.shape, i)
+    # print('\n',type(embeddings), embeddings.shape, i)
     # for e in embeddings:
     #     print(e)
+    with open(f'results/{modelName}/embeddings/embeddings{i}.txt', 'w') as file:
+        for line in embeddings:
+            file.write(f'{line}\n')
+
     similarities = model.similarity(embeddings, embeddings)
-    print(similarities)
-    print('------------------------------------------------------------------------------------------------------------')
-    
+    with open(f'results/{modelName}/similarities/similarities{i}.txt', 'w') as file:
+        file.write(str(similarities))
+
     # heatmaps
     plt.figure(figsize=(6, 5))
     plt.imshow(similarities, cmap='Blues', interpolation='none')
@@ -50,7 +58,7 @@ for i in range(0, len(documents), 4):
     plt.yticks(ticks=np.arange(len(chunk)), labels=vector_labels)
 
     plt.tight_layout()
-    plt.savefig(f'gte-base/plot{i}.png')  # Save plot to file
-
+    plt.savefig(f'results/{modelName}/plots/plot{i}.png')  # Save plot to file
+    plt.close()
 
 
