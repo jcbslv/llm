0ec13a6f456b94e7b8213dcb9edf648b6c588907
jcb <0x0jcb@gmail.com>
2025-01-07 14:17:46 -0600

    testing embeddings

--- single.py
+++ single.py
@@ -1,4 +1,4 @@
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, SimilarityFunction
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,8 +9,8 @@ modelName = str(
     # "Mihaiii/Ivysaur")
     # "Mihaiii/Bulbasaur")
     # "Mihaiii/Venusaur")
-    # 'intfloat/e5-small-v2')
-    'intfloat/e5-base-v2')
+    'intfloat/e5-small-v2')
+    # 'intfloat/e5-base-v2')
     # 'intfloat/e5-large-v2')
     # 'Snowflake/snowflake-arctic-embed-l-v2.0')
     # 'sentence-transformers/average_word_embeddings_glove.6B.300d')
@@ -28,40 +28,42 @@ model = SentenceTransformer(modelName)
     # trust_remote_code=True,
     # device="cpu",
     # config_kwargs={"use_memory_efficient_attention": False, "unpad_inputs": False})
-
+model.similarity_fn_name = SimilarityFunction.COSINE
 with open('Justifications1Col.txt', 'r') as file:
     documents = file.readlines()
 
 documents = [line.strip() for line in documents]
 
 
-embeddings = model.encode(documents, normalize_embeddings=True)
-print('\n',type(embeddings), embeddings.shape)
+embeddings = model.encode(documents, normalize_embeddings=False)
+# print('\n',type(embeddings), embeddings.shape)
     # for e in embeddings:
     #     print(e)
 for i in range(0, len(documents), 4):
     # with open(f'singleResults/{modelName}/embeddings/embeddings{i}.txt', 'w') as file:
     #     for line in embeddings:
     #         file.write(f'{line}\n')
-    embMatrix = embeddings[i::(i+4)]
+    embMatrix = embeddings[i:(i+4)]
+    print('\n',type(embMatrix), embMatrix.shape)
+    # print(embMatrix)
 
-    # similarities = model.similarity(embMatrix, embMatrix)
-    # with open(f'singleResults/{modelName}/similarities/similarities{i}.txt', 'w') as file:
-    #     file.write(str(similarities))
+    similarities = model.similarity(embMatrix, embMatrix)
+    with open(f'singleResults/{modelName}/similarities/similarities{i}.txt', 'w') as file:
+        file.write(str(similarities))
 
     # heatmaps
-    # plt.figure(figsize=(6, 5))
-    # plt.imshow(similarities, cmap='Blues', interpolation='none')
-    # plt.colorbar(label='Cosine Similarity')
-    # plt.title('Cosine Similarity Heatmap')
+    plt.figure(figsize=(6, 5))
+    plt.imshow(similarities, cmap='Blues', interpolation='none')
+    plt.colorbar(label='Cosine Similarity')
+    plt.title('Cosine Similarity Heatmap')
 
     # vector labels
-    # vector_labels = ['Justification 1', 'Justification 2', 'Justification 3', 'Justification 4']
-    # plt.xticks(ticks=np.arange(len(embMatrix)), labels=vector_labels, rotation=45)
-    # plt.yticks(ticks=np.arange(len(embMatrix)), labels=vector_labels)
+    vector_labels = ['Justification 1', 'Justification 2', 'Justification 3', 'Justification 4']
+    plt.xticks(ticks=np.arange(len(embMatrix)), labels=vector_labels, rotation=45)
+    plt.yticks(ticks=np.arange(len(embMatrix)), labels=vector_labels)
 
-    # plt.tight_layout()
-    # plt.savefig(f'singleResults/{modelName}/plots/plot{i}.png')  # Save plot to file
-    # plt.close()
+    plt.tight_layout()
+    plt.savefig(f'singleResults/{modelName}/plots/plot{i}.png')  # Save plot to file
+    plt.close()
 
 
